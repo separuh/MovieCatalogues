41d3acf06fc8ff391263147a58af328bdb8baafe
separuh <imamdham@gmail.com>
2022-06-27 10:00:21 +0700

    TUGAS 9

---
+++ app/src/main/java/com/wew/moviecatalogues/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.wew.moviecatalogues
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.wew.moviecatalogues.R
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    var movies: List<Movie>? = null
+    private lateinit var movieAdapter: MovieAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rv_movies_list.layoutManager = LinearLayoutManager(this)
+        rv_movies_list.setHasFixedSize(true)
+        getMovieData { movies : List<Movie> -> rv_movies_list.adapter = MovieAdapter(movies, object : MovieAdapter.OnAdapterListener {
+            override fun onClick(result: Movie) { val intent = Intent(applicationContext, DetailMovieActivity::class.java)
+                intent.putExtra(DetailMovieActivity.EXTRA_DATA, result)
+                startActivity(intent)
+            }
+        })
+        }
+    }
+    private fun setupRecyclerView(){
+        movieAdapter = MovieAdapter(arrayListOf(), object : MovieAdapter.OnAdapterListener {
+            override fun onClick(result: Movie) { val intent = Intent(applicationContext, DetailMovieActivity::class.java)
+                intent.putExtra(DetailMovieActivity.EXTRA_DATA, result)
+                startActivity(intent)
+            }
+        })
+        rv_movies_list.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = movieAdapter
+        }
+    }
+    private fun getMovieData(callback: (List<Movie>) -> Unit){
+        val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
+        apiService.getMovieList().enqueue(object : Callback<MovieResponse> {
+            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
+
+            }
+            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
+                movies = response.body()!!.movies
+
+                return callback(response.body()!!.movies)
+
+            }
+        })
+    }
+}
